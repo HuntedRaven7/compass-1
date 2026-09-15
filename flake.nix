0004b665b50a2bcd1a59dde3ce93dea07e09383f
Robin Groves <EdwardGroves2007@proton.me>
2026-09-15 03:19:34 -0400

    fmt: test

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default";
-    crane.url = "github:ipetkov/crane/v0.17.0";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane = {
+      url = "github:ipetkov/crane/v0.17.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     soulver-cpp = {
       url = "github:vicinaehq/soulver-cpp";
       inputs.nixpkgs.follows = "nixpkgs";
