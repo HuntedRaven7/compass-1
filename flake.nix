0663372ba1f3568307716960a07ed4cc70b69ed8
Robin Groves <EdwardGroves2007@proton.me>
2026-09-15 04:09:05 -0400

    fix: darwin support

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
           description = "Vicinae Rust engine";
           homepage = "https://github.com/tuna-os/compass";
           license = lib.licenses.gpl3Plus;
-          platforms = with lib.platforms; linux;
+            platforms = with lib.platforms; linux ++ darwin;
         };
       };
   in {
